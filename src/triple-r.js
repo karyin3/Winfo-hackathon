13d506db192bb7b9ba1f67d0dbb085cb9fd67b93
Christine Wolf <christinewolf@D-69-91-177-76.dhcp4.washington.edu>
2017-01-28 13:54:55 -0800

    added single page functionality

--- src/triple-r.js
+++ src/triple-r.js
@@ -1,5 +1,6 @@
 (function() {
 	window.onload = function() {
+		loadHomePage();
 		document.getElementById("search").onclick = findItem;
 		document.getElementById("home").onclick = loadHomePage;
 		document.getElementById("recycle").onclick = loadRecyclePage;
@@ -49,21 +50,41 @@
 	function findItem() {
 		//var data = require('./itemData.json');
 		//var data = JSON.parse(this.responseText);
-		var info = '{"cans": {"category": "recycle"},"shoeboxes": {"category": "recycle"}}';
+		var info = '{ "cans": { "category": "Recycle", "instructions": "No preparation required", "DIY": { "DIY-pic": "http://www.diyncrafts.com/wp-content/uploads/2016/02/recycle-repurpose-soda-cans-easy-projects.jpg", "DIY-link": "http://www.diyncrafts.com/17586/repurpose/20-ways-to-recycle-soda-cans", "DIY-title": "20 Genius Ways to Recycle Soda Cans into Amazing DIY Projects" } }, "shoeboxes": { "category": "recycle" } } ';
 		var data = JSON.parse(info);
 		var item = document.getElementById("items").value;
 		console.log(data);
 		console.log(item);
-		console.log(info[item]);
-		loadItemPage(info, item);
+		console.log(data[item].category);
+		loadItemPage(data[item]);
     }
 
-    function loadItemPage(info, item) {
+    function loadItemPage(info) {
     	document.getElementById("home-page").style.display = "none";
 		document.getElementById("recycle-page").style.display = "none";
 		document.getElementById("compost-page").style.display = "none";
 		document.getElementById("landfill-page").style.display = "none";
 		document.getElementById("single-page").style.display = "block";
+
+		var options = ["category", "instructions", "recycle-number", "DIY"];
+		for (var i = 0; i < options.length; i++) {
+			if (info[options[i]]) {
+				if (options[i] !== "DIY") {
+					document.getElementById(options[i]).innerHTML = info[options[i]];
+				} else {
+					if (info.DIY["DIY-pic"]) {
+						document.getElementById("DIY-pic").src = info.DIY["DIY-pic"];
+					}
+					if (info.DIY["DIY-link"] && info.DIY["DIY-title"]) {
+						console.log(info.DIY["DIY-link"]);
+						document.getElementById("DIY-link").href = info.DIY["DIY-link"];
+						document.getElementById("DIY-link").innerHTML = info.DIY["DIY-title"];
+					}
+				}
+			} else {
+				document.getElementById(options[i] + '-div').style.display = "none";
+			}
+		}
     }
 
 })();
