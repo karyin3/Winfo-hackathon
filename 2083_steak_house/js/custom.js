68728c37c6bfd823da1015e11986e1c993415466
Deanna <deannahaha@D-173-250-187-244.dhcp4.washington.edu>
2017-01-28 13:22:39 -0800

    updated html

--- 2083_steak_house/js/custom.js
+++ 2083_steak_house/js/custom.js
@@ -102,9 +102,9 @@ $(document).ready(function() {
   $(function(){
     jQuery(document).ready(function() {
     $('#home').backstretch([
-       "images/home-bg-slideshow1.jpg", 
-       "images/home-bg-slideshow2.jpg",
-       "images/home-bg-slideshow3.jpg",
+       "images/compost.jpg", 
+       "images/recycle.jpg",
+       "images/newspaper.jpg",
         ],  {duration: 2000, fade: 750});
     });
   })
