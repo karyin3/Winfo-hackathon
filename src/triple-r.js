92d7e96bd53a65830e05b82e348648c67200fdad
Christine Wolf <christinewolf@D-69-91-177-76.dhcp4.washington.edu>
2017-01-28 12:32:39 -0800

    naming conventions

--- src/triple-r.js
+++ src/triple-r.js
@@ -1,6 +1,10 @@
 (function() {
 	window.onload = function() {
 		document.getElementById("search").onclick = findItem;
+		document.getElementById("home").onclick = loadHomePage;
+		document.getElementById("recycle").onclick = loadRecyclePage;
+		document.getElementById("compost").onclick = loadCompostPage;
+		document.getElementById("landfill").onclick = loadLandfillPage;
 	}
 
 	// function requestItem() {
@@ -9,6 +13,38 @@
 	// 	request.open("GET", "./itemData.json");
 	// 	request.send();
 	// }
+
+	function loadHomePage() {
+		document.getElementById("home-page").style.display = "block";
+		document.getElementById("recycle-page").style.display = "none";
+		document.getElementById("compost-page").style.display = "none";
+		document.getElementById("landfill-page").style.display = "none";
+		document.getElementById("single-page").style.display = "none";
+	}
+
+	function loadRecyclePage() {
+		document.getElementById("home-page").style.display = "none";
+		document.getElementById("recycle-page").style.display = "block";
+		document.getElementById("compost-page").style.display = "none";
+		document.getElementById("landfill-page").style.display = "none";
+		document.getElementById("single-page").style.display = "none";
+	}
+
+	function loadCompostPage() {
+		document.getElementById("home-page").style.display = "none";
+		document.getElementById("recycle-page").style.display = "none";
+		document.getElementById("compost-page").style.display = "block";
+		document.getElementById("landfill-page").style.display = "none";
+		document.getElementById("single-page").style.display = "none";
+	}
+
+	function loadLandfillPage() {
+		document.getElementById("home-page").style.display = "none";
+		document.getElementById("recycle-page").style.display = "none";
+		document.getElementById("compost-page").style.display = "none";
+		document.getElementById("landfill-page").style.display = "block";
+		document.getElementById("single-page").style.display = "none";
+	}
 
 	function findItem() {
 		//var data = require('./itemData.json');
@@ -23,7 +59,11 @@
     }
 
     function loadItemPage(info, item) {
-
+    	document.getElementById("home-page").style.display = "none";
+		document.getElementById("recycle-page").style.display = "none";
+		document.getElementById("compost-page").style.display = "none";
+		document.getElementById("landfill-page").style.display = "none";
+		document.getElementById("single-page").style.display = "block";
     }
 
 })();
