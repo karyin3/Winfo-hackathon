1cf72d8c8cbfcd26ddc3007e19503fc54feb4d8e
Christine Wolf <christinewolf@D-69-91-177-76.dhcp4.washington.edu>
2017-01-28 12:05:34 -0800

    silly page

--- src/triple-r.js
+++ src/triple-r.js
@@ -1,19 +1,29 @@
 (function() {
 	window.onload = function() {
-		document.getElementById("search").onclick = requestItem;
+		document.getElementById("search").onclick = findItem;
 	}
 
-	function requestItem() {
-		var request = new XMLHttpRequest();
-		request.onload = findItem;
-		request.open("GET", "/itemData.json");
-		request.send();
-	}
+	// function requestItem() {
+	// 	var request = new XMLHttpRequest();
+	// 	request.onload = findItem;
+	// 	request.open("GET", "./itemData.json");
+	// 	request.send();
+	// }
 
 	function findItem() {
-		var data = JSON.parse(this.responseText);
+		//var data = require('./itemData.json');
+		//var data = JSON.parse(this.responseText);
+		var info = '{"cans": {"category": "recycle"},"shoeboxes": {"category": "recycle"}}';
+		var data = JSON.parse(info);
+		var item = document.getElementById("items").value;
+		console.log(data);
+		console.log(item);
+		console.log(info[item]);
+		loadItemPage(info, item);
     }
-});
-	}
+
+    function loadItemPage(info, item) {
+
+    }
 
 })();
