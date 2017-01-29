48f2c52fe8837ef1e7e1f751513d98c5d160d8df
Christine Wolf <christinewolf@D-69-91-177-76.dhcp4.washington.edu>
2017-01-28 16:05:06 -0800

    stuff

--- 2083_steak_house/js/comblefill.js
+++ 2083_steak_house/js/comblefill.js
@@ -32,7 +32,6 @@
 	}
 
 	function loadCompostPage() {
-		console.log("compost");
 		document.getElementById("main-page").style.display = "none";
 		document.getElementById("recycle-page").style.display = "none";
 		document.getElementById("compost-page").style.display = "";
@@ -49,7 +48,6 @@
 	}
 
 	function findItem() {
-		console.log("item");
 		//var data = require('./itemData.json');
 		//var data = JSON.parse(this.responseText);
 		var info = '{ "cans": { "category": "Recycle", "instructions": "No preparation required.", "DIY": { "DIY-pic": "http://www.diyncrafts.com/wp-content/uploads/2016/02/recycle-repurpose-soda-cans-easy-projects.jpg", "DIY-link": "http://www.diyncrafts.com/17586/repurpose/20-ways-to-recycle-soda-cans", "DIY-title": "20 Genius Ways to Recycle Soda Cans into Amazing DIY Projects" } }, "shoeboxes": { "category": "Recycle", "instructions": "Flatten box first.", "DIY": { "DIY-pic": "http://www.diyncrafts.com/wp-content/uploads/2015/08/1-picnic-box.jpg", "DIY-link": "http://www.diyncrafts.com/14195/repurpose/25-brilliantly-crafty-shoebox-projects-for-you-your-home-and-the-kids", "DIY-title": "25 Brilliantly Crafty Shoebox Projects for You, Your Home, and the Kids" } }, "newspapers": { "category": "Compost or Recycle", "instructions": "If composting, shred first. Do not recycle wet or soiled newspaper.", "DIY": { "DIY-pic": "http://craftingagreenworld.com/wp-content/uploads/2014/07/30-awesome-diy-projects-using-newspaper.jpg", "DIY-link": "http://craftingagreenworld.com/2014/07/24/30-awesome-diy-projects-using-newspaper/", "DIY-title": "30 Awesome DIY Projects Using Newspaper!" } }, "paper plates": { "category": "Compost", "instructions": "No preparation required." }, "wine bottles": { "category": "Recycle", "instructions": "Rinse first, and if selling may need to be grouped by color", "DIY": { "DIY-pic": "http://clv.h-cdn.co/assets/15/32/1438723749-knotted-jute-netted-bottles-026.jpg", "DIY-link": "http://www.countryliving.com/diy-crafts/g2534/repurpose-wine-bottles/?slide=1", "DIY-title": "23 Creative Ways to Repurpose Your Empty Wine Bottles" } }, "yogurt cups": { "category": "Recycle", "instructions": "Wash out first. Also may not be accepted by curbside recycling in some areas.", "recycle-number": "Yogurt cups are normally number 5, polypropylene, which can be recycled but sometimes requires special facilities.", "DIY": { "DIY-pic": "https://www.thisoldhouse.com/sites/default/files/styles/content_box/public/migrated/images/02-reuse-containers.jpg?itok=DN_r22sn&timestamp=1464282283", "DIY-link": "https://www.thisoldhouse.com/ideas/10-uses-yogurt-cups", "DIY-title": "10 Uses for Yogurt Cups" } }, "plastic bottles": { "category": "Recycle", "instructions": "Rinse first if contained liquid other than water", "DIY": { "DIY-pic": "http://static.boredpanda.com/blog/wp-content/uploads/2014/07/plastic-bottles-recycling-ideas-11.jpg", "DIY-link": "http://www.boredpanda.com/plastic-bottle-recycling-ideas/", "DIY-title": "23 Creative Ways To Reuse Old Plastic Bottles" } } }';
@@ -61,18 +59,17 @@
     }
 
     function loadItemPage(info, item) {
-    	console.log(info);
-    	var result = "";
-    	result += item.charAt(0).toUpperCase();
-    	for (var i = 1; i < item.length; i++) {
-    		if (item.charAt(i) == " " && i + 1 < item.length) {
-    			i++;
-    			result += " " + item.charAt(i).toUpperCase();
-    		} else {
-    			result += item.charAt(i);
-    		}
-    	}
-    	console.log(result);
+    	//var result = "";
+    	// result += item.charAt(0).toUpperCase();
+    	// for (var i = 1; i < item.length; i++) {
+    	// 	if (item.charAt(i) == " " && i + 1 < item.length) {
+    	// 		i++;
+    	// 		result += " " + item.charAt(i).toUpperCase();
+    	// 	} else {
+    	// 		result += item.charAt(i);
+    	// 	}
+    	// }
+    	document.getElementById("single-title").innerHTML = item;
 
     	document.getElementById("main-page").style.display = "none";
 		document.getElementById("recycle-page").style.display = "none";
@@ -90,7 +87,6 @@
 						document.getElementById("DIY-pic").src = info.DIY["DIY-pic"];
 					}
 					if (info.DIY["DIY-link"] && info.DIY["DIY-title"]) {
-						console.log(info.DIY["DIY-link"]);
 						document.getElementById("DIY-link").href = info.DIY["DIY-link"];
 						document.getElementById("DIY-link").innerHTML = info.DIY["DIY-title"];
 					}
